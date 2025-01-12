5159cb1becf93c75aa840d8c6beecdb0e534db8a
Haricarbajal <haricarbajal3@gmail.com>
2025-01-12 02:43:09 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base : "https://github.com/Haricarbajal/Practica7React",
   plugins: [react()],
 })
